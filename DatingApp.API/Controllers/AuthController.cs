84f15204f47337d44d1816866e14beacd4fc69bc
Shaswat Anandhan <shas_anan@yahoo.com>
2020-07-16 02:05:16 +0400

    RoleMgmt JWT Token Working

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -82,7 +82,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(new
             {
-                token = GenerateJwtToken(user),
+                token = GenerateJwtToken(user).Result,
                 user = appUser
             });
                 }
